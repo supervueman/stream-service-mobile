85c5f51d77a8c04657dbde9361d887f1ecff62f6
supervueman <supervueman@gmail.com>
2019-06-22 15:47:28 +0400

    Edits

--- app/App.vue
+++ app/App.vue
@@ -1,7 +1,27 @@
 <template>
   <Page class="page">
-    <ActionBar title="Profile" class="toolbar"></ActionBar>
-    <router-view></router-view>
+    <ActionBar title="Profile" class="toolbar"></ActionBar>>
+    <GridLayout rows="*, auto">
+      <StackLayout row="0">
+        <router-view></router-view>
+      </StackLayout>
+
+      <BottomNavigation
+        activeColor="#ff32b0"
+        inactiveColor="#3a5bff"
+        backgroundColor="#030823"
+        row="1"
+        titleVisibility="never"
+        @tabPressed="tabPressed"
+        @tabSelected="onBottomNavigationTabSelected"
+        v-if="profile._id !== ''"
+      >
+        <BottomNavigationTab title="Profile" icon="earth"/>
+        <BottomNavigationTab title="Profile" icon="groupprofileusers"/>
+        <BottomNavigationTab title="Profile" icon="product"/>
+        <BottomNavigationTab title="Profile" icon="profile"/>
+      </BottomNavigation>
+    </GridLayout>
   </Page>
 </template>
 
@@ -15,9 +35,32 @@ export default {
       password: ""
     };
   },
+  computed: {
+    profile() {
+      return this.$store.getters["authenticate/getProfile"];
+    }
+  },
   methods: {
-    selectedTab(args) {
-      console.log(args.value);
+    tabPressed(args) {
+      console.log(args);
+    },
+    onBottomNavigationTabSelected({ newIndex }) {
+      switch (newIndex) {
+        case 0:
+          this.$router.push("/streams");
+          break;
+        case 1:
+          this.$router.push("/users");
+          break;
+        case 2:
+          this.$router.push("/products");
+          break;
+        case 3:
+          this.$router.push("/profile");
+          break;
+        default:
+          break;
+      }
     }
   },
   async mounted() {
--- app/main.js
+++ app/main.js
@@ -6,16 +6,22 @@ import apolloProvider from '@/plugins/apolloProvider';
 import '@/assets/sass/index.sass';
 import VueDevtools from 'nativescript-vue-devtools';
 import localStorage from 'nativescript-localstorage';
+import BottomNavigation from 'nativescript-bottom-navigation/vue';
+
 
 if (TNS_ENV !== 'production') {
   Vue.use(VueDevtools);
 }
+
+Vue.use(BottomNavigation)
 
 // Prints Vue logs when --env.production is *NOT* set while building
 Vue.config.silent = (TNS_ENV === 'production');
 
 Vue.registerElement('Carousel', () => require('nativescript-carousel').Carousel);
 Vue.registerElement('CarouselItem', () => require('nativescript-carousel').CarouselItem);
+// Vue.registerElement('BottomNavigation', () => require('nativescript-bottom-navigation/vue').BottomNavigation);
+// Vue.registerElement('BottomNavigationTab', () => require('nativescript-bottom-navigation/vue').BottomNavigationTab);
 
 new Vue({
   store,
